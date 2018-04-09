ae88ad72ce7dc55a184f0465b9573822d1d841dc
namazaci <34474282+namazaci@users.noreply.github.com>
2018-04-09 17:29:36 +0200

    Adding force to the ball

--- Brick Breaker Game/Assets/Scripts/Ball.cs
+++ Brick Breaker Game/Assets/Scripts/Ball.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	public Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
+
+		rb.AddForce(Vector2.up * 500);
 	}
 	
 	// Update is called once per frame

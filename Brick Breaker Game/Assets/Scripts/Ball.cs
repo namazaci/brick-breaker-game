28b6bbb4f1e48f4ae9072c035faec06cb5338dc4
namazaci <34474282+namazaci@users.noreply.github.com>
2018-04-09 18:35:37 +0200

    Adding a child object to the paddle and editing the Ball Script
    For the script we edited it so that the ball and the paddle would move together and start with spacebar or a click.
    
    A child object is created so that the position of the ball wouldn't be in the paddle

--- Brick Breaker Game/Assets/Scripts/Ball.cs
+++ Brick Breaker Game/Assets/Scripts/Ball.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
 	public Rigidbody2D rb;
+	public bool inPlay;
+	public Transform paddle;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +17,14 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(!inPlay)
+		{
+			transform.position = paddle.position;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if(other.CompareTag("bottom"))
+		if(other.CompareTag("Bottom"))
 		{
 			Debug.Log("Ball hit the bottom of the screen");	
 		}

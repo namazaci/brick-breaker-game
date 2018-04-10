2566c1e783c824c60957e23516dbe9a198c60221
namazaci <34474282+namazaci@users.noreply.github.com>
2018-04-10 13:50:53 +0200

    Adding the lives and scores text to the Game Manager

--- Brick Breaker Game/Assets/Scripts/GameManager.cs
+++ Brick Breaker Game/Assets/Scripts/GameManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+	public int lives;
+	public int score;
+	public Text livesText;
+	public Text scoreText;
+	
 	// Use this for initialization
 	void Start () {
 		
